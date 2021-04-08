ea6540c9ee1b6f945f8097703ab33680b018ffed
Marcoulakis <femarcoulakis@gmail.com>
2021-04-08 13:51:45 -0300

    enabled CORS

--- src/index.js
+++ src/index.js
@@ -11,3 +11,26 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const allowCors = fn =&gt; async (req, res) =&gt; {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) =&gt; {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
