22eb6d3bdb878e7cab4574bf1fea1599252b2b71
Marcoulakis <femarcoulakis@gmail.com>
2021-04-08 12:55:49 -0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './styles/index.css';
-// import { AppRouter } from './router';
-// import "../src/main.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import { AppRouter } from './router';
+import "../src/main.scss";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <AppRouter />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
